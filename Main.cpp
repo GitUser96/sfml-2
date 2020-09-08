c87a0b7f84f5f50b3f482a8134594edc3e66aa5d
GitUser96 <maiswu.1996@gmail.com>
2020-09-08 09:00:40 +0200

    First commit.

--- Main.cpp
+++ Main.cpp
@@ -1,32 +1,63 @@
-#include "Student.h"
-#include <fstream>
-using namespace std;
+#include "Player.h"
 
 void main()
 {
+	int arr[ROWS][COLUMNS];
+	// Reading from File
 	ifstream inputFile;
-	inputFile.open("StudentsData.txt");
-	if (!inputFile.fail())
+	inputFile.open("Board.txt");
+	if (inputFile.is_open())
 	{
-		// Read the data of students from a file into an array of class Student.
-		Student arr[3];
-		int i = 0;
-		string tempName;
-		int tempID;
-		while (!inputFile.eof() && (i < 3))
-		{
-			inputFile >> tempName;
-			inputFile >> tempID;
-			arr[i].setName(tempName);
-			arr[i].setID(tempID);
-			i++;
-		}
+		for (int i = 0; i < ROWS; i++)
+			for (int j = 0; j < COLUMNS; j++)
+				if (!inputFile.eof())
+					inputFile >> arr[i][j];
 		inputFile.close();
+	}
+	else
+		cout << "Error loading Board.txt ..." << endl;
 
-		// Print the data of the students.
-		for (int i = 0; i < 3; i++)
+	// SFML
+	RenderWindow window;
+	window.create(VideoMode(800, 800), "Simple Maze");
+
+	Player p(35, 1, 1, "CuteFace.png");
+	Texture bricksTexture, grassTexture;
+	bricksTexture.loadFromFile("Bricks.png");
+	grassTexture.loadFromFile("Grass.png");
+	RectangleShape board[ROWS][COLUMNS];
+	for (int i = 0; i < ROWS; i++)
+		for (int j = 0; j < COLUMNS; j++)
 		{
-			arr[i].print();
+			board[i][j].setSize(Vector2f(70, 70));
+			board[i][j].setPosition(50 + 70 * j, 50 + 70 * i);
+			if (arr[i][j] < 0)
+			{
+				board[i][j].setTexture(&bricksTexture);
+				board[i][j].setFillColor(Color::Yellow);
+			}
+			else
+			{
+				board[i][j].setTexture(&grassTexture);
+				board[i][j].setFillColor(Color::Green);
+			}
 		}
+	Event e;
+	while (window.isOpen())
+	{
+		while (window.pollEvent(e))
+		{
+			if (e.type == Event::Closed)
+				window.close();
+			else if (e.type == Event::KeyPressed)
+				p.moveOnWindow(e, arr);
+		}
+		window.clear();
+		for (int i = 0; i < ROWS; i++)
+			for (int j = 0; j < COLUMNS; j++)
+				window.draw(board[i][j]);
+		// window.draw(p.getPlayer());
+		p.drawOnWindow(window);
+		window.display();
 	}
 }
